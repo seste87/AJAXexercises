6ed8bdafdcfa0265c7ba4773bacfb0a276489305
Nicolas James Hampton <nicolasjhampton@gmail.com>
2015-08-12 14:47:35 -0700

    3rd class

--- AJAX_Basics_Project_Files stage 2/stage2/video5/js/widget.js
+++ AJAX_Basics_Project_Files stage 2/stage2/video5/js/widget.js
@@ -1,13 +1,8 @@
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
-  if(xhr.readyState === 4) {
-    console.log(typeof xhr.responseText);
+  if (xhr.readyState === 4) {
     var employees = JSON.parse(xhr.responseText);
-    console.log(typeof employees);
-    console.log(employees);
-    
-  }
-  
+   }
 };
 xhr.open('GET', 'data/employees.json');
 xhr.send();
